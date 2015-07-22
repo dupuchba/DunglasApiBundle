125a999ae26d6057794fdca9ad16306bd46e5c16
Kévin Dunglas <dunglas@gmail.com>
2015-07-22 08:39:04 +0200

    Fix tests

--- Tests/Mapping/Loader/AnnotationLoaderTest.php
+++ Tests/Mapping/Loader/AnnotationLoaderTest.php
@@ -38,20 +38,27 @@ class AnnotationLoaderTest extends \PHPUnit_Framework_TestCase
         $readerProphecy->getPropertyAnnotation($reflectionProperty, AnnotationLoader::IRI_ANNOTATION_NAME)->willReturn($attributeAnnotation)->shouldBeCalled();
         $reader = $readerProphecy->reveal();
 
-        $attributeMetadataProphecy = $this->prophesize('Dunglas\ApiBundle\Mapping\AttributeMetadataInterface');
-        $attributeMetadataProphecy->getName()->willReturn('attr')->shouldBeCalled();
-        $attributeMetadataProphecy->setIri($attributeAnnotation->value)->shouldBeCalled();
-        $attributeMetadata = $attributeMetadataProphecy->reveal();
+        $attributeMetadata2 = $this->prophesize('Dunglas\ApiBundle\Mapping\AttributeMetadataInterface')->reveal();
 
-        $classMetadataProphecy = $this->prophesize('Dunglas\ApiBundle\Mapping\ClassMetadataInterface');
-        $classMetadataProphecy->getReflectionClass()->willReturn($reflectionClass)->shouldBeCalled();
-        $classMetadataProphecy->setIri($classAnnotation->value)->shouldBeCalled();
-        $classMetadataProphecy->getAttributes()->willReturn([$attributeMetadata])->shouldBeCalled();
-        $classMetadata = $classMetadataProphecy->reveal();
+        $attributeMetadataProphecy1 = $this->prophesize('Dunglas\ApiBundle\Mapping\AttributeMetadataInterface');
+        $attributeMetadataProphecy1->withIri($attributeAnnotation->value)->willReturn($attributeMetadata2)->shouldBeCalled();
+        $attributeMetadata1 = $attributeMetadataProphecy1->reveal();
+
+        $classMetadata3 = $this->prophesize('Dunglas\ApiBundle\Mapping\ClassMetadataInterface')->reveal();
+
+        $classMetadataProphecy2 = $this->prophesize('Dunglas\ApiBundle\Mapping\ClassMetadataInterface');
+        $classMetadataProphecy2->getAttributesMetadata()->willReturn(['attr' => $attributeMetadata1])->shouldBeCalled();
+        $classMetadataProphecy2->withAttributeMetadata('attr', $attributeMetadata2)->willReturn($classMetadata3)->shouldBeCalled();
+        $classMetadata2 = $classMetadataProphecy2->reveal();
+
+        $classMetadataProphecy1 = $this->prophesize('Dunglas\ApiBundle\Mapping\ClassMetadataInterface');
+        $classMetadataProphecy1->getReflectionClass()->willReturn($reflectionClass)->shouldBeCalled();
+        $classMetadataProphecy1->withIri($classAnnotation->value)->willReturn($classMetadata2)->shouldBeCalled();
+        $classMetadata1 = $classMetadataProphecy1->reveal();
 
         $loader = new AnnotationLoader($reader);
 
         $this->assertInstanceOf('Dunglas\ApiBundle\Mapping\Loader\LoaderInterface', $loader);
-        $loader->loadClassMetadata($classMetadata, ['a'], ['b'], ['c']);
+        $this->assertEquals($classMetadata3, $loader->loadClassMetadata($classMetadata1, ['a'], ['b'], ['c']));
     }
 }
